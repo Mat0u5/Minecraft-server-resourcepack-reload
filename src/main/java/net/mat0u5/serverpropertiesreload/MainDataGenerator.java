09631fa450cc125dbd5e8e8aa9b79e0988b9ff37
Mat0u5 <matous.danko+github@gmail.com>
2024-06-06 18:49:43 +0200

    Setting up stuff

--- src/main/java/net/mat0u5/serverpropertiesreload/MainDataGenerator.java
+++ src/main/java/net/mat0u5/serverpropertiesreload/MainDataGenerator.java
@@ -3,7 +3,7 @@ package net.mat0u5.serverpropertiesreload;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class MinecraftServerpropertiesReloadDataGenerator implements DataGeneratorEntrypoint {
+public class MainDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
