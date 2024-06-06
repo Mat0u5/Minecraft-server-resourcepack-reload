13290c12bd89617a4ec5e6914df3ef7b48d9246a
Mat0u5 <matous.danko+github@gmail.com>
2024-06-06 18:11:26 +0200

    Initial Commit

---
+++ src/main/java/net/mat0u5/serverpropertiesreload/MinecraftServerpropertiesReloadDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mat0u5.serverpropertiesreload;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinecraftServerpropertiesReloadDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
