b0d85711acdc875c3c36a72acbf0c1d3741fe3a5
Mat0u5 <matous.danko+github@gmail.com>
2024-06-07 23:43:35 +0200

    Command perms and structure finished (for now)

--- src/main/java/net/mat0u5/serverresourcepackreload/command/Properties.java
+++ src/main/java/net/mat0u5/serverresourcepackreload/command/Properties.java
@@ -1,9 +1,13 @@
 package net.mat0u5.serverresourcepackreload.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
@@ -11,6 +15,7 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.*;
 import net.minecraft.server.MinecraftServer.ServerResourcePackProperties;
+import org.spongepowered.include.com.google.common.collect.ImmutableList;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -19,13 +24,17 @@ public class Properties {
     public static void register(CommandDispatcher<ServerCommandSource> serverCommandSourceCommandDispatcher,
                                 CommandRegistryAccess commandRegistryAccess,
                                 CommandManager.RegistrationEnvironment registrationEnvironment) {
-        serverCommandSourceCommandDispatcher.register(CommandManager.literal("resource-pack")
-                .then(CommandManager.literal("reload").executes(Properties::reload)));
-        serverCommandSourceCommandDispatcher.register(CommandManager.literal("resource-pack")
-                .then(CommandManager.literal("reloadForAllPlayers").executes(Properties::reload)));
+        /*serverCommandSourceCommandDispatcher.register(CommandManager.literal("resourcepack")
+                .then(CommandManager.literal("reload").executes(Properties::reload)));*/
+
+//.then(CommandManager.argument("url", StringArgumentType.string()).then(CommandManager.argument("sha1", StringArgumentType.string())
+        serverCommandSourceCommandDispatcher.register((LiteralArgumentBuilder)((LiteralArgumentBuilder)((LiteralArgumentBuilder)CommandManager.literal("resourcepack").then(CommandManager.literal("reload").executes(context -> Properties.execute((ServerCommandSource)context.getSource(), ImmutableList.of(((ServerCommandSource)context.getSource()).getPlayerOrThrow())))).then(CommandManager.literal("reload").then(CommandManager.argument("targets", EntityArgumentType.players()).requires(source -> source.hasPermissionLevel(2)).executes(context -> Properties.execute((ServerCommandSource)context.getSource(), EntityArgumentType.getPlayers(context, "targets"))))))));
+        serverCommandSourceCommandDispatcher.register((LiteralArgumentBuilder)((LiteralArgumentBuilder)((LiteralArgumentBuilder)CommandManager.literal("resourcepack")
+                .then(CommandManager.literal("custom").requires(source -> source.hasPermissionLevel(2)).then(CommandManager.argument("url", StringArgumentType.string()).then(CommandManager.argument("sha1", StringArgumentType.string()).executes(context -> Properties.executeApplyCustom((ServerCommandSource)context.getSource(), ImmutableList.of(((ServerCommandSource)context.getSource()).getPlayerOrThrow()), StringArgumentType.getString(context, "url"), StringArgumentType.getString(context, "sha1")))
+                .then(CommandManager.argument("targets", EntityArgumentType.players()).executes(context -> Properties.executeApplyCustom((ServerCommandSource)context.getSource(), EntityArgumentType.getPlayers(context, "targets"), StringArgumentType.getString(context, "url"), StringArgumentType.getString(context, "sha1"))))))))));
     }
-    public static int reload(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        final ServerCommandSource source = context.getSource();
+    public static int execute(ServerCommandSource source, Collection<? extends PlayerEntity> targets) throws CommandSyntaxException {
+        //final ServerCommandSource source = context.getSource();
         final PlayerEntity self = source.getPlayer();
         MinecraftServer server = source.getServer();
 
@@ -42,7 +51,20 @@ public class Properties {
         server.getPlayerManager().broadcast(Text.translatable("Resource pack url: " + resourcePackURL),false);
         server.getPlayerManager().broadcast(Text.translatable("Resource pack sha1: " + resourcePackSHA1),false);
         //self.sendMessage(Text.translatable("Message To Player"));
-        reloadResourcePack(server, self, newRPurl,newRPSHA1);
+        for(PlayerEntity player : targets) {
+            reloadResourcePack(server, player, newRPurl,newRPSHA1);
+        }
+        return 1;
+    }
+    public static int executeApplyCustom(ServerCommandSource source, Collection<? extends PlayerEntity> targets, String newRPurl, String newRPSHA1) throws CommandSyntaxException {
+        MinecraftServer server = source.getServer();
+
+
+        server.getPlayerManager().broadcast(Text.translatable("Applying custom RP_URL: " + newRPurl),false);
+        server.getPlayerManager().broadcast(Text.translatable("Applying custom RP_SHA: " + newRPSHA1),false);
+        for(PlayerEntity player : targets) {
+            reloadResourcePack(server, player, newRPurl,newRPSHA1);
+        }
         return 1;
     }
     private static ServerResourcePackProperties getServerResourcePackProperties(MinecraftServer server) {
