40da0bc04a31f92afd1e97db90d7f9c7b25890eb
Mat0u5 <matous.danko+github@gmail.com>
2024-07-10 12:31:27 +0200

    v.1.0.4\nNo longer checks for new resource packs manually, has to recieve a command instead.

--- src/main/java/net/mat0u5/serverresourcepackreload/events/Events.java
+++ src/main/java/net/mat0u5/serverresourcepackreload/events/Events.java
@@ -11,12 +11,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 
 public class Events {
 
-    private static final int RESOURCE_PACK_UPDATE_CHECK_INTERVAL_TICKS = 20 * 60; // 60 seconds in ticks
-    private static int tickCounter = 0;
-
     public static void register() {
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> onPlayerJoin(server, handler.getPlayer()));
-        ServerTickEvents.END_SERVER_TICK.register((server) -> onServerTick(server));
     }
 
     private static void onPlayerJoin(MinecraftServer server, ServerPlayerEntity player) {
@@ -29,14 +25,5 @@ public class Events {
                 if (player.hasPermissionLevel(2)) Resourcepack.sendNewRPMessage(player);
             }
         }catch(Exception e){}
-    }
-
-    private static void onServerTick(MinecraftServer server) {
-        tickCounter++;
-        if (tickCounter >= RESOURCE_PACK_UPDATE_CHECK_INTERVAL_TICKS) {
-            tickCounter = 0;
-            Main.config.loadProperties();
-            Resourcepack.checkResourcepackUpdate(server);
-        }
     }
 }
